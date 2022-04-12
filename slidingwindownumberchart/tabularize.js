30a217554cdb182d77b8fae88e1bec470bd7cc7f
jsyang <jsyang.ca@gmail.com>
2022-04-12 15:16:32 +0100

    add print and window

--- slidingwindownumberchart/tabularize.js
+++ slidingwindownumberchart/tabularize.js
@@ -8,16 +8,26 @@ function getTableFromNumberChart(chartRows) {
 }
 
 const digitMap = [null, X_DIGITS, XX_DIGITS, XXX_DIGITS, XXXX_DIGITS];
+const digitLegend = '万千十一';
 
 function updateChartDisplay() {
     const chartRows = generateType1Chart(digitMap[chartSettings.digits], chartSettings.colSize);
     document.getElementById('chartContainer').innerHTML = getTableFromNumberChart(chartRows);;
+    document.getElementById('windowContainer').innerHTML =
+        '<table id=window>' +
+        `<tr>${'<td class=slot>0</td>'.repeat(chartSettings.digits)}</tr>` +
+        `<tr>${Array.from(digitLegend).reverse().slice(0, chartSettings.digits).reverse()
+            .map(c => `<td class=legend>${c}</td>`)
+            .join('')
+        }</tr>` +
+        '</table>';
+
 }
 
 function updateChartFont() {
     const sheet = Array.from(document.styleSheets)
         .find(ss => {
-            if(!ss || !ss.href) return;
+            if (!ss || !ss.href) return;
 
             return ss.href.indexOf('index') >= 0
         });
@@ -29,4 +39,11 @@ function updateChartFont() {
 window.onload = () => {
     updateChartDisplay();
     updateChartFont();
+};
+
+window.onmousemove = e => {
+    const wStyle= document.getElementById('window').style;
+    wStyle.left = e.clientX + 'px';
+    wStyle.top = e.clientY + 'px';
+    wStyle.position = 'absolute';
 };
